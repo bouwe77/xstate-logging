41b2ec96dddb614a54cba2bc76a03fb72ee83e34
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-06-20 17:24:56 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useInterpret } from "@xstate/react";
+import { useInterpret , useMachine} from "@xstate/react";
 import { actions, assign, createMachine, spawn } from "xstate";
 import { send } from "xstate/lib/actions";
 
@@ -7,7 +7,7 @@ const { log } = actions;
 const myMachine = createMachine(
   {
     id: "myMachine",
-    entry: "spawnAnotherMachine",
+    entry: "spawnLogMachine",
     on: {
       DO_SOMETHING: {
         actions: [log("hello"), "logViaSpawnedMachine"]
@@ -16,7 +16,10 @@ const myMachine = createMachine(
   },
   {
     actions: {
-      logViaSpawnedMachine: (ctx) => send("LOG", { to: ctx.anotherMachineRef })
+      logViaSpawnedMachine: (ctx) => {
+        console.log(ctx.ref)
+        return send("LOG", { to: ctx.ref })
+      }
     }
   }
 );
@@ -34,15 +37,18 @@ export default function App() {
   const service = useInterpret(myMachine, {
     actions: {
       spawnLogMachine: assign({
-        logMachineRef: () => spawn(logMachine)
+        ref: () => spawn(logMachine, 'mySpawnedLogMachine')
       })
     },
     logger: console.info
   });
 
+  const [,send] = useMachine(logMachine)
+
   return (
     <>
       <button onClick={() => service.send("DO_SOMETHING")}>Click here</button>
+      <button onClick={() => send("LOG")}>Log directly</button>
     </>
   );
 }
